4bdec31f3f1c05d99656ea5a6d215ce73b608da9
Олексадр Ткаленко <mr.undesired@gmail.com>
2024-01-29 19:11:39 +0200

    finish questions block homePage

--- src/Container/Sections/HomeSections/HomeQuestions/HomeQuestions.tsx
+++ src/Container/Sections/HomeSections/HomeQuestions/HomeQuestions.tsx
@@ -2,13 +2,19 @@ import { Container } from '@mui/material'
 import './HomeQuestions.scss'
 import HomeQuestionAccordion from './HomeQuestionsAccordion/HomeQuestionAccordion'
 import HomeQuestionTitle from './HomeQuestionTitle/HomeQuestionTitle'
+import questionsLogo from 'assets/images/HomeQuestions/pngegg.svg'
 
 interface Props {}
 const HomeQuestions = (props: Props) => {
     return (
-        <Container>
-            <HomeQuestionTitle />
-            <HomeQuestionAccordion />
+        <Container className="home-questions-container">
+            <div className="home-questions-content">
+                <HomeQuestionTitle />
+                <HomeQuestionAccordion />
+            </div>
+            <div className="home-questions-logo">
+                <img src={questionsLogo} alt="questionsLogo" />
+            </div>
         </Container>
     )
 }
